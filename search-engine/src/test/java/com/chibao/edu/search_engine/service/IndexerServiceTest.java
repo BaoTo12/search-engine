23eadc2c6c5c9d74a5bd254f84f66be1c579483d
ChiBao <baotochi87@gmail.com>
2025-10-29 18:12:07 +0700

    add IndexerServiceTest

---
+++ search-engine/src/test/java/com/chibao/edu/search_engine/service/IndexerServiceTest.java
@@ -0,0 +1,24 @@
+package com.chibao.edu.search_engine.service;
+
+import com.chibao.edu.search_engine.repository.WebPageRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class IndexerServiceTest {
+
+    @Mock
+    private WebPageRepository webPageRepository;
+
+    @InjectMocks
+    private IndexerService indexerService;
+
+    @Test
+    void testTokenizeContent() throws Exception {
+        // This would test the Lucene tokenization logic
+        // Example: verify stopwords are removed, stemming is applied
+    }
+}
