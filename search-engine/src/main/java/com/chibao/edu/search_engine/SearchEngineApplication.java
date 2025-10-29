5dcdfb018e3662dd1a768ff403c2d43a70bb5e6e
ChiBao <baotochi87@gmail.com>
2025-10-29 18:05:23 +0700

    add application properties and annotations EnableJpaRepositories, EnableScheduling, EnableKafka, EnableCaching, EnableElasticsearchRepositories in main application class

--- search-engine/src/main/java/com/chibao/edu/search_engine/SearchEngineApplication.java
+++ search-engine/src/main/java/com/chibao/edu/search_engine/SearchEngineApplication.java
@@ -2,12 +2,24 @@ package com.chibao.edu.search_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableKafka
+@EnableScheduling
+@EnableAsync
+@EnableCaching
+@EnableJpaRepositories(basePackages = "com.chibao.edu.search_engine.repository")
+@EnableElasticsearchRepositories(basePackages = "com.chibao.edu.search_engine.repository")
 public class SearchEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearchEngineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineApplication.class, args);
+    }
 
 }
