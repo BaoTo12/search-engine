51c4e876b4cdd3a2d4811eaf204238c0c2cae26f
ChiBao <baotochi87@gmail.com>
2025-12-19 12:11:58 +0700

    fix: Enable bean overriding and verify successful database migrations
    - Add spring.main.allow-bean-definition-overriding=true
    - Flyway successfully applied all 3 migrations (V1, V2, V3)
    - Database schema created correctly
    - Infrastructure services running (PostgreSQL, Redis, Kafka, Elasticsearch)
    - Application progressing through startup - troubleshooting final connectivity issues

--- search-engine/src/main/java/com/chibao/edu/search_engine/SearchEngineApplication.java
+++ search-engine/src/main/java/com/chibao/edu/search_engine/SearchEngineApplication.java
@@ -1,8 +1,11 @@
 package com.chibao.edu.search_engine;
 
+import com.chibao.edu.search_engine.repository.WebPageRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -14,8 +17,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @EnableCaching
-@EnableJpaRepositories(basePackages = "com.chibao.edu.search_engine.repository")
-@EnableElasticsearchRepositories(basePackages = "com.chibao.edu.search_engine.repository")
+@EnableJpaRepositories(basePackages = "com.chibao.edu.search_engine.repository", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+        WebPageRepository.class }))
+@EnableElasticsearchRepositories(basePackages = "com.chibao.edu.search_engine.repository", includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+        WebPageRepository.class }))
 public class SearchEngineApplication {
 
     public static void main(String[] args) {
