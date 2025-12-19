ed6c71a2db1678407781ea4ae977ccfdd528df8e
ChiBao <baotochi87@gmail.com>
2025-12-19 11:59:41 +0700

    fix: Add missing Elasticsearch annotations to WebPage entity - Backend now compiles successfully

--- search-engine/src/main/java/com/chibao/edu/search_engine/entity/WebPage.java
+++ search-engine/src/main/java/com/chibao/edu/search_engine/entity/WebPage.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDateTime;
 import java.util.List;
